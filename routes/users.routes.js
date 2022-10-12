8cdd4268ac3d28ced60995c0a1f88dbca41d847c
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-10-12 17:19:26 +0200

    falta por acabar itineration 3

---
+++ routes/users.routes.js
@@ -0,0 +1,92 @@
+const router = require("express").Router()
+
+const User = require("../models/User.model.js")
+
+const bcrypt = require("bcryptjs")
+const saltRounds = 10
+
+// Vista de users/login
+router.get("/login", (req, res, next)=>{
+    res.render("users/login");
+})
+
+// Vista cuando rellenas el formulario de users/login: si está bien te lleva a users/profile, sino a users/login
+router.post("/login", (req, res, next)=>{
+    const { username, password } = req.body;
+
+    User.findOne({username})
+    .then(user => { 
+        if(bcrypt.compareSync(password, user.password)) {
+            req.session.currentUser=user;   
+            res.redirect("/users/profile");
+        } else {
+            const data = {missatgeError: "Incorrect credentials"};
+            if(req.session.currentUser) data.username = req.session.currentUser.username;
+            res.render("users/login", data)
+        }
+    })
+    .catch(err => {
+        res.render("error", {missatgeError: err})   
+    })
+})
+
+// Vista de users/singup
+router.get("/signup", (req, res, next)=>{
+    res.render("users/signup");
+})
+
+// Vista cuando te registras: si todo va bien te lleva a users/profile, si hay algo mal, users/signup
+router.post("/signup", (req, res, next)=>{
+    const {username, password, passwordRepeat} = req.body;
+
+    if(!username || !password  || !passwordRepeat) {
+        const data = {missatgeError: "Missing fields"};
+        if(req.session.currentUser) data.username = req.session.currentUser.username;
+        
+        res.render("users/signup", data);
+        return; 
+    }
+    if(password != passwordRepeat) {
+        const data = {missatgeError: "Diferent passwords"};
+        if(req.session.currentUser) data.username = req.session.currentUser.username;
+
+        res.render("users/signup", data);
+        return;
+    }
+    if (username === username) {
+        const data = { missatgeError: "This username is already created" }
+        if (req.session.currentUser) data.username = req.session.currentUser.username;
+        
+        res.render("users/signup", data);
+        return;
+    }
+
+    // Para encriptar el password
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const passwordHash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        username,
+        password: passwordHash
+    })
+    .then(result => {
+        req.session.currentUser=result;   
+        res.redirect("/users/profile");     
+    })
+    .catch(err => {
+        const data = {missatgeError: err};
+        if(req.session.currentUser) data.username = req.session.currentUser.username;
+
+        res.render("error", data)   
+    })
+})
+
+// Para ver la vista de users/profile
+router.get("/profile", (req, res, next)=>{
+    const data = {missatgeError: "Diferent passwords"};
+    if(req.session.currentUser) data.username = req.session.currentUser.username;
+
+    res.render("users/profile", data);    
+});
+
+module.exports = router
